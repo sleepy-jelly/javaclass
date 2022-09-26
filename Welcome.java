0b0ed7133c51778e01c8744c65b8e2f132c78ff2
zayle_nothing <57306207+zayle-nothing@users.noreply.github.com>
2022-09-26 12:29:35 +0900

    Add files via upload

---
+++ Welcome.java
@@ -0,0 +1,19 @@
+
+
+public class Welcome 
+{
+	
+	public static void main(String[] args) 
+	{
+		int num =3;
+		int max =9;
+	   /* for(int i=1;i<max+1;i++) {
+	   	System.out.println((num)+" x "+(i) +" = "+(num*i));
+	   } */
+	    for(int i=1;i<max+1;i++) {
+	    	int sum= i*num;
+	    	System.out.println((num)+" x "+(i) +" = "+sum);
+	    }
+	}
+
+}

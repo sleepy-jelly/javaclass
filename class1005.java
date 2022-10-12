05d2f55d0035dff19294c87213212cce5f8f1d9f
sleepy-jelly <57306207+sleepy-jelly@users.noreply.github.com>
2022-10-12 17:43:22 +0900

    Add files via upload

--- class1005.java
+++ class1005.java
@@ -1,35 +1,41 @@
+
 import java.util.Scanner;
+
 public class class1005 {
 
 	public static void main(String[] args) {
 		class1005Student st1= new class1005Student();
 		Scanner sc = new Scanner(System.in);
+        st1.RESET();                        /// st1 객체 내부 배열 초기화 작업
+
+
 
 		boolean flag=true;
 		boolean flag2=true;
 		
-		int nPointer = 0;
+		int nStudentID = 0;
 		
 		while(flag) {
 			System.out.println("Student Mangement Program");
 			System.out.println();
-			System.out.println("Setting by pointer-----------1");
-			System.out.println("Setting by fullModule--------2");
-			System.out.println("Showing by pointer-----------3");
-			System.out.println("Showing by fullModule--------4");
+			System.out.println("Setting by studentID-----------1");
+			System.out.println("Setting by Module------------2");
+			System.out.println("Showing by studentID-----------3");
+			System.out.println("Showing by Module------------4");
 			System.out.println("Exit-------------------------5");
 			System.out.println();
 			int nTemp =sc.nextInt();
 			if(nTemp==1) {
+
 				flag2=true;
 				while(flag2){
-				System.out.println("         Setting by pointer              ");
+				System.out.println("         Setting by studentID              ");
 				System.out.println();
-				System.out.println("Setting student number by pointer-1");
-				System.out.println("Setting student name by pointer---2");
-				System.out.println("Setting kor score by pointer------3");
-				System.out.println("Setting eng score by pointer------4");
-				System.out.println("Setting math score by pointer-----5");
+				System.out.println("Setting student number by studentID-1");
+				System.out.println("Setting student name by studentID---2");
+				System.out.println("Setting kor score by studentID------3");
+				System.out.println("Setting eng score by studentID------4");
+				System.out.println("Setting math score by studentID-----5");
 				System.out.println("Exit-------------------------6");
 				System.out.println();
 				
@@ -37,32 +43,34 @@ public class class1005 {
 				switch(nTempIn) {
 				case 1:
 					System.out.println("input studenet number, pointer(integer)");
-					int tempStuNum = sc.nextInt();
-					nPointer = sc.nextInt();
-					st1.setStuNum(tempStuNum, nPointer);
+					int nStudentID1 = sc.nextInt();
+					st1.setstuId(nStudentID1);
 					break;
 				case 2:
-					
+                    System.out.println("input studenet Name, pointer(integer)");
+                    String nTempName = sc.next();
+                    nStudentID = sc.nextInt();
+                    st1.setName(nTempName, nStudentID);
 					break;
 				case 3:
 					System.out.println("input kor score, pointer(integer)");
 					int kor = sc.nextInt();
-					nPointer = sc.nextInt();
-					st1.setStuNum(kor, nPointer);
+					nStudentID = sc.nextInt();
+					st1.setKor(kor, nStudentID);
 					break;
 				case 4:
 					System.out.println("input eng score, pointer(integer)");
 					int eng = sc.nextInt();
-					nPointer = sc.nextInt();
-					st1.setStuNum(eng, nPointer);
+					nStudentID = sc.nextInt();
+					st1.setEng(eng, nStudentID);
 					break;
 					
 					
 				case 5:
 					System.out.println("input math score, pointer(integer)");
 					int math = sc.nextInt();
-					nPointer = sc.nextInt();
-					st1.setStuNum(math, nPointer);
+					nStudentID = sc.nextInt();
+					st1.setMath(math, nStudentID);
 					break;
 					
 				case 6:
@@ -79,7 +87,8 @@ public class class1005 {
 			}
 				
 			}else if(nTemp==2) {
-				System.out.println("       Setting by fullModule              ");
+
+				System.out.println("       Setting by Module              ");
 				System.out.println();
 				flag2=true;
 				while(flag2){
@@ -90,12 +99,23 @@ public class class1005 {
 					int nTempIn=sc.nextInt();
 					switch(nTempIn) {
 					case 1:
-						
-						
+                        System.out.println("input name, student number, kor , eng, math, pointer");
+                        String newName=sc.next();
+                        int newStudentNumber=sc.nextInt();
+                        int newKorScore =sc.nextInt();
+                        int newEngScore = sc.nextInt();
+                        int newMathScore = sc.nextInt();
+						st1.setFull(newName, newStudentNumber, newKorScore, newEngScore, newMathScore);
 						break;
 					case 2:
-						
-						
+						System.out.println("Reset all setting? type 1 to confirm");
+                        int finalCheck= sc.nextInt();
+                        if(finalCheck==1){
+                           st1.RESET();
+                            System.out.println("Reseted all setting");
+                        }else{
+                            System.out.println("Reset canceled");
+                        }
 						break;
 					case 3:
 						System.out.println("              exiting              ");
@@ -110,13 +130,160 @@ public class class1005 {
 				}
 				
 			}else if(nTemp==3) {
-				
-				
-			}else if(nTemp==4){
-				
+                System.out.println("Showing by studentID-----------3");
+                System.out.println();
+                flag2=true;
+                while(flag2){
+                    System.out.println("         Showing by studentID              ");
+				    System.out.println();
+				    System.out.println("Show student number by studentID-1");
+				    System.out.println("Show student name by studentID---2");
+			    	System.out.println("Show kor score by studentID------3");
+			    	System.out.println("Show eng score by studentID------4");
+			    	System.out.println("Show math score by studentID-----5");
+                    System.out.println("Show average score by studentID--6");
+                    System.out.println("Show total score by studentID----7");
+			    	System.out.println("Exit---------------------------8");
+			    	System.out.println();
+                    
+                    int newSelcect =sc.nextInt();
+                    int nStudentID2=0;
+
+                    switch(newSelcect){
+                        case 1:
+                        System.out.println("Show student number by studentID");
+                        System.out.println();
+                        System.out.println("Input StudentID(integer)");
+
+                        nStudentID2=sc.nextInt();
+                        System.out.println("student number: "+st1.getstuId(nStudentID2));
+                        break;
+
+                        case 2:
+                        System.out.println("Show student name by studentID");
+                        System.out.println();
+                        System.out.println("Input StudentID(integer)");
+
+
+                        nStudentID2=sc.nextInt();
+                        System.out.println("student name: "+st1.getName(nStudentID2));
+                        break;
+
+                        case 3:
+                        System.out.println("Show kor score by studentID------3");
+                        System.out.println();
+                        System.out.println("Input StudentID(integer)");
+
+
+                        nStudentID2=sc.nextInt();
+                        System.out.println("kor score : "+st1.getKor(nStudentID2));
+                        break;
+
+
+                        case 4:
+                        System.out.println("Show eng score by studentID------4");
+                        System.out.println();
+                        System.out.println("Input StudentID(integer)");
+
+
+                        nStudentID2=sc.nextInt();
+                        System.out.println("eng score : "+ st1.getEng(nStudentID2));
+                        break;
+
+
+                        case 5:
+                        System.out.println("Show math score by studentID-----5");
+                        System.out.println();
+                        System.out.println("Input StudentID(integer)");
+
+
+                        nStudentID2=sc.nextInt();
+                        System.out.println("math score : "+st1.getMath(nStudentID2));
+                        break;
+
+
+                        case 6:
+                        System.out.println("Show average score by studentID--6");
+                        System.out.println();
+                        System.out.println("Input StudentID(integer)");
+
+
+                        nStudentID2=sc.nextInt();
+                        System.out.println("average score : "+st1.getAvg(nStudentID2));
+                        break;
+
+
+                        case 7: 
+                        System.out.println("Show total score by studentID----7");
+                        System.out.println();
+                        System.out.println("Input StudentID(integer)");
+
+
+                        nStudentID2=sc.nextInt();
+                        System.out.println("total score : "+st1.getSum(nStudentID2));
+                        break;
+
+
+                        case 8:
+                        System.out.println("Exit");
+                        System.out.println();
+
+                        flag2=false;
+                        break;
+
+                        
+                        default:
+                        System.out.println("       value is out of range           ");
+						System.out.println();
+
+                        break;
+                    }
+
+
+
+                }
+
+			}else if(nTemp==4) {
+                System.out.println("Showing by Module");
+                System.out.println();
+                flag2=true;
+                while(flag2){
+                    System.out.println("Showing by Module----1");
+                    System.out.println("Exit---------------------2");
+                    int nSelect= sc.nextInt();
+                    switch(nSelect){
+                        case 1:
+                            System.out.println("Input StudentID(Integer)");
+                            System.out.println();
+                            int nStudentID2=sc.nextInt();
+                            st1.printFullByString(nStudentID2);
+                            
+
+                        break;
+
+                        case 2:
+                        System.out.println("Exiting.........");
+                        System.out.println();
+
+
+                        flag2=false;
+                        break;
+
+                        default :
+                        System.out.println("       value is out of range           ");
+						System.out.println();
+
+                        break;
+                    }
+
+                }
+
+
 			}else if(nTemp==5) {
 				System.out.println("         End of program              ");
 				System.out.println();
+
+
 				flag= false;
 			}else {
 				System.out.println("input value is out of range");
@@ -127,6 +294,8 @@ public class class1005 {
 			
 			
 		}
+		
+		st1.printNumStud();
 		
 		sc.close();
 	}
--- class1005Student.java
+++ class1005Student.java
@@ -1,27 +1,52 @@
+
+
 import java.util.Scanner;
 
 public class class1005Student {
 	// define 
-	static Scanner sc = new Scanner(System.in);
-	static StringBuilder sb = new StringBuilder();
 	
-	private static final int MAX =10; // MAX == array MAXimum range 
+	class1005Student(){
+		RESET();
+	}
+	class1005Student(int studentId){
+		setstuId(studentId);
+		RESET();
+		
+	}
+	
+	
+	
+	
+	static StringBuilder sb= new StringBuilder();
+	static Scanner sc = new Scanner(System.in);
+	
+	private static final int MAX =100; // MAX == array MAXimum range 
 	private static final int MAXSCORE = 100;         // MAXimum value of score
 	private static final int MINSCORE = 0;			// MINimum value of score
-	private int[] stuNum;
+	private int[] stuId= new int[MAX];
 	
-	private String[] name;
+	private String[] name = new String[MAX];
+	private String[] address = new String[MAX];
+	private String[] sex = new String[MAX];
+	private int[] birthday = new int[MAX];
 	
-	private int[] kor;
-	private int[] eng;
-	private int[] math;
 	
-	private double[] avg;
-	private int[] sum;
+	private int[] kor =new int[MAX];
+	private int[] eng =new int[MAX];
+	private int[] math =new int[MAX];
 	
+	
+	
+	private double[] avg= new double[MAX];
+	private int[] sum= new int[MAX];
+	
+	
+	private int cnt =0;
+	private int[] pointerA= new int[MAX];
+
 	protected void RESET() {                // reset all data
 		for(int i=0; i<MAX;i++) {
-			this.stuNum[i]=0;
+			this.stuId[i]=0;
 			this.name[i]="null";
 			this.kor[i] =0;
 			this.eng[i] =0;
@@ -30,15 +55,33 @@ public class class1005Student {
 			this.sum[i] = 0;
 		}
 	}
+	
 	///// set by multiple object module
-	protected void setFull(String name_,int stuNum_, int kor_, int eng_,int math_, int pointer) {
+	protected void setFull(String name_,int stuId_, int kor_, int eng_,int math_) {
+		
+		setstuId(stuId_);
+
+		int pointer= 0;
+		boolean idChecker = true;
+		
+		for(int i =0; i<MAX;i++) {
+			if(stuId[i]==stuId_) {
+				pointer =cnt;
+				idChecker =false;
+				break;
+			}
+		}
+		
+		
+		if(idChecker==true) {
+			System.out.println("  unvalid data");
+			return;
+		}
 		
 		///data checking start
-		if(this.name[pointer]!="null") {
-			System.out.println("pointer(location): "+pointer+" has already data over there do you want over write? ");
+		if(cnt<MAX) {
+			System.out.println("student number(location): "+cnt+" has already data over   do you want over write? ");
 			System.out.println("if you want to over write type '1'");
-			
-			
 			
 			if(sc.nextInt()==1) {
 				
@@ -59,7 +102,7 @@ public class class1005Student {
 		}
 		
 		
-		this.stuNum[pointer]=stuNum_;
+		this.stuId[pointer]=stuId_;
 		this.name[pointer] = name_;
 	
 		
@@ -87,7 +130,24 @@ public class class1005Student {
 	}
 	
 	///// set by single object module
-	protected void setName(String name_,int pointer) {
+	protected void setName(String name_,int stuId_) {
+		int pointer= 0;
+		boolean idChecker = true;
+		
+		for(int i =0; i<MAX;i++) {
+			if(stuId[i]==stuId_) {
+				pointer =cnt;
+				idChecker =false;
+				break;
+			}
+		}
+		
+		
+		if(idChecker==true) {
+			System.out.println("  unvalid data");
+			return;
+		}
+		
 		if(pointer>=0&&pointer<MAX) {
 		this.name[pointer] =name_;
 		}else {
@@ -96,30 +156,68 @@ public class class1005Student {
 		}
 	}
 	
-	protected void setStuNum(int stuNum_,int pointer) {
-		if(stuNum_<0||MAX<stuNum_) {
-			
-			System.out.println("stuNum is out of range");
-			return;
-		}else if(pointer>=0&&pointer<MAX) {
-			this.stuNum[pointer] =stuNum_;
-			}else {
-				System.out.println("pointer is out of range");
-				return;
-			}
+	protected void setstuId(int stuId_) {
+		int pointer =cnt;
 		
-		this.stuNum[pointer]=stuNum_;
+		if(pointer>=0&&pointer<MAX) {
+			this.stuId[pointer] =stuId_;
+		}else {
+			System.out.println("pointer is out of range");
+			return;
+		}
+		this.pointerA[cnt] = cnt;
+		this.stuId[pointer]=stuId_;
+		cnt++;
 	}
 
-	protected void setKor(int kor_,int pointer) {
+	protected void setKor(int kor_,int stuId_) {
+		int pointer= 0;
+		boolean idChecker = true;
+		
+		for(int i =0; i<MAX;i++) {
+			if(stuId[i]==stuId_) {
+				pointer =cnt;
+				idChecker =false;
+				break;
+			}
+		}
+		
+		
+		if(idChecker==true) {
+			System.out.println("  unvalid data");
+			return;
+		}
+		
+		
 		if((kor_<MINSCORE&&MAXSCORE>kor_)&&(pointer<MAX&&pointer>=0)) {
 			System.out.println("kor is out of range");
 			return;
 		}
+		
 		this.kor[pointer]=kor_;
 	}
-
-	protected void setEng(int eng_,int pointer) {
+	
+	
+	protected void setEng(int eng_,int stuId_) {
+		
+		int pointer= 0;
+		boolean idChecker = true;
+		
+		for(int i =0; i<MAX;i++) {
+			if(stuId[i]==stuId_) {
+				pointer =cnt;
+				idChecker =false;
+				break;
+			}
+		}
+		
+		
+		if(idChecker==true) {
+			System.out.println("  unvalid data");
+			return;
+		}
+		
+		
 		if((eng_<MINSCORE&&MAXSCORE>eng_)&&(pointer<MAX&&pointer>=0)) {
 			System.out.println("eng is out of range");
 			return;
@@ -127,7 +225,25 @@ public class class1005Student {
 		this.eng[pointer]=eng_;
 	}
 
-	protected void setMath(int math_,int pointer) {
+	protected void setMath(int math_,int stuId_) {
+		
+		int pointer= 0;
+		boolean idChecker = true;
+		
+		for(int i =0; i<MAX;i++) {
+			if(stuId[i]==stuId_) {
+				pointer =cnt;
+				idChecker =false;
+				break;
+			}
+		}
+		
+		
+		if(idChecker==true) {
+			System.out.println("  unvalid data");
+			return;
+		}
+		
 		if((math_<MINSCORE&&MAXSCORE>math_)&&(pointer<MAX&&pointer>=0)) {
 			System.out.println("math is out of range");
 			return;
@@ -135,41 +251,251 @@ public class class1005Student {
 		this.math[pointer]=math_;
 	}
 	
+	/**
+	 * @return the address
+	 */
+	public String getAddress(int stuId_) {
+		int pointer= 0;
+		boolean idChecker = true;
+		
+		for(int i =0; i<MAX;i++) {
+			if(stuId[i]==stuId_) {
+				pointer =cnt;
+				idChecker =false;
+				break;
+			}
+		}
+		
+		
+		if(idChecker==true) {
+			System.out.println("  unvalid data");
+			return"null";
+		}
+		return this.address[pointer];
+	}
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address, int stuId_) {
+		int pointer= 0;
+		boolean idChecker = true;
+		
+		for(int i =0; i<MAX;i++) {
+			if(stuId[i]==stuId_) {
+				pointer =cnt;
+				idChecker =false;
+				break;
+			}
+		}
+		
+		
+		if(idChecker==true) {
+			System.out.println("  unvalid data");
+			return;
+		}
+		this.address[pointer] = address;
+	}
 	//////////////////////////////////////////////////  get
-	protected String getName(int pointer) {
+	protected String getName(int stuId_) {
+		
+		int pointer= 0;
+		boolean idChecker = true;
+		
+		for(int i =0; i<MAX;i++) {
+			if(stuId[i]==stuId_) {
+				pointer =cnt;
+				idChecker =false;
+				break;
+			}
+		}
+		
+		
+		if(idChecker==true) {
+			System.out.println("  unvalid data");
+			return "null";
+		}
+		
 		return this.name[pointer];
 	}
 
-	protected int getStuNum(int pointer) {
-		return this.stuNum[pointer];
+	protected int getstuId(int stuId_) {
+		
+
+		int pointer= 0;
+		boolean idChecker = true;
+		
+		for(int i =0; i<MAX;i++) {
+			if(stuId[i]==stuId_) {
+				pointer =cnt;
+				idChecker =false;
+				break;
+			}
+		}
+		
+		
+		if(idChecker==true) {
+			System.out.println("  unvalid data");
+			return -1;
+		}
+		
+		return this.stuId[pointer];
 	}
 	
-	protected int getKor(int pointer) {
+	protected int getKor(int stuId_) {
+		
+		int pointer= 0;
+		boolean idChecker = true;
+		
+		for(int i =0; i<MAX;i++) {
+			if(stuId[i]==stuId_) {
+				pointer =cnt;
+				idChecker =false;
+				break;
+			}
+		}
+		
+		
+		if(idChecker==true) {
+			System.out.println("  unvalid data");
+			return -1;
+		}
+		
 		return this.kor[pointer];
 	}
 	
-	protected int getEng(int pointer) {
+	protected int getEng(int stuId_) {
+		
+		int pointer= 0;
+		boolean idChecker = true;
+		
+		for(int i =0; i<MAX;i++) {
+			if(stuId[i]==stuId_) {
+				pointer =cnt;
+				idChecker =false;
+				break;
+			}
+		}
+		
+		
+		if(idChecker==true) {
+			System.out.println("  unvalid data");
+			return -1;
+		}
+		
 		return this.eng[pointer];
 	}
 	
-	protected int getMath(int pointer) {
+	protected int getMath(int stuId_) {
+		
+		int pointer= 0;
+		boolean idChecker = true;
+		
+		for(int i =0; i<MAX;i++) {
+			if(stuId[i]==stuId_) {
+				pointer =cnt;
+				idChecker =false;
+				break;
+			}
+		}
+		
+		if(idChecker==true) {
+			System.out.println("  unvalid data");
+			return -1;
+		}
+		
 		return this.math[pointer];
 	}
 	
-	protected double getAvg(int pointer) {
+	protected double getAvg(int stuId_) {
+		
+		int pointer= 0;
+		boolean idChecker = true;
+		
+		for(int i =0; i<MAX;i++) {
+			if(stuId[i]==stuId_) {
+				pointer =cnt;
+				idChecker =false;
+				break;
+			}
+		}
+		
+		if(idChecker==true) {
+			System.out.println("  unvalid data");
+			return -1;
+		}
+		
+		
 		this.avg[pointer]=(this.kor[pointer]+this.eng[pointer]+this.math[pointer])/3;
 		return avg[pointer];
 	}
 
-	protected int getSum(int pointer) {
+	protected int getSum(int stuId_) {
+		
+		int pointer= 0;
+		boolean idChecker = true;
+		
+		for(int i =0; i<MAX;i++) {
+			if(stuId[i]==stuId_) {
+				pointer =cnt;
+				idChecker =false;
+				break;
+			}
+		}
+		
+		if(idChecker==true) {
+			System.out.println("  unvalid data");
+			return -1;
+		}
+		
 		this.sum[pointer] =(this.kor[pointer]+this.eng[pointer]+this.math[pointer]);
 		return sum[pointer];
 	}
 
-	protected String getFullByString(int pointer) {
-		
-		
-		return "a";
-	}
-}
+	protected void printFullByString(int stuId_) {
 
+		int pointer= 0;
+		boolean idChecker = true;
+		
+		for(int i =0; i<MAX;i++) {
+			if(stuId[i]==stuId_) {
+				pointer =cnt;
+				idChecker =false;
+				break;
+			}
+		}
+		
+		if(idChecker==true) {
+			System.out.println("  unvalid data");
+			return;
+		}
+		
+		sb.setLength(0);
+		
+		sb.append("학번: "+Integer.toString(stuId[pointer])+" ");
+		
+		
+		sb.append("이름: "+this.name[pointer]+" ");
+		
+		sb.append("한국어: "+Integer.toString(kor[pointer])+" ");
+		
+		sb.append("영어: "+Integer.toString(eng[pointer])+" ");
+		
+		sb.append("수학: "+Integer.toString(math[pointer])+" ");
+		getAvg(pointer);
+		
+		sb.append("평균: "+Double.toString(Math.round(getAvg(pointer)*100)/100)+" ");
+		
+		
+		getSum(pointer);
+		
+		sb.append("총점수: "+Integer.toString(sum[pointer]));
+		System.out.println(sb);
+		return;
+	}
+
+	
+	protected void printNumStud() {
+		System.out.println(cnt);
+	}
+	
+}
